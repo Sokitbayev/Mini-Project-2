b041d4eba2c6307ac42b996ec89018746a35b5b3
Sokitbayev <46749286+Sokitbayev@users.noreply.github.com>
2019-11-08 18:58:29 +0600

    Update script.js

--- Mini-Project-2/script.js
+++ Mini-Project-2/script.js
@@ -2,3 +2,21 @@ function calcNumbers(result){
     form.displayResult.value=form.displayResult.value+result;
 
 }
+function dis(val) 
+         { 
+             document.getElementById("result").value+=val 
+         } 
+           
+         //function that evaluates the digit and return result 
+         function solve() 
+         { 
+             let x = document.getElementById("result").value 
+             let y = eval(x) 
+             document.getElementById("result").value = y 
+         } 
+           
+         //function that clear the display 
+         function clr() 
+         { 
+             document.getElementById("result").value = "" 
+         } 
